90835aab5e90acb7bf2db064eb970c4d325101be
Alina Kvasova <al.kvasovaa@yandex.ru>
2022-10-24 18:44:20 +0700

    вывод формы в консоль, слайдер

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,51 @@
-// import $ from 'jquery';
+
+import 'slick-carousel/slick/slick';
+import '@fancyapps/ui';
+import $ from 'jquery';
+
+function init () {
+    $('.slider').slick({
+        slidesToShow: 4,
+        prevArrow: '.slider-prev',
+        nextArrow: '.slider-next',
+        infinite: false,
+    
+
+
+
+
+    responsive: [
+        {
+          breakpoint: 1300,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 680,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    })
+
+
+
+}
+
+
+
+
+init ();
