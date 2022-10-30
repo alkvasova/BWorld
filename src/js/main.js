acba74d83b9cc116f6f2d579d40f8b3dd632359f
Alina Kvasova <al.kvasovaa@yandex.ru>
2022-10-30 23:33:38 +0700

    добавила маску и валидацию

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,6 @@ function retrieveFormValue (event) {
 }
 
 form.addEventListener ('submit', retrieveFormValue);
+
+
+
---
+++ src/js/mask.js
@@ -0,0 +1,11 @@
+
+let phoneInput = document.querySelector(".phone");
+let btn = document.querySelector(".popup_button");
+
+const phoneMask = new IMask(phoneInput, {
+  mask: "+{7}(000)000-00-00",
+});
+
+
+
+
